ebd4c16c06e2625940cb1b813b9486284c13ac87
Ian Cárdenas C <icardenac@bancoripley.com>
2021-08-16 23:30:22 -0400

    Documentar y Comentarios - Se documenta urils - Se documenta models

--- libs/models/src/lib/models.ts
+++ libs/models/src/lib/models.ts
@@ -1,9 +1,21 @@
+/**
+ * Blog que contiene posts
+ *
+ * @export
+ * @interface IBlog
+ */
 export interface IBlog {
   posts: IPost[];
   category: string;
   ads: boolean;
 }
 
+/**
+ * Post del blog
+ *
+ * @export
+ * @interface IPost
+ */
 export interface IPost {
   title: string;
   description: string;
@@ -11,7 +23,14 @@ export interface IPost {
   author: IAuthor[];
   tags: string;
 }
-
+/**
+ * Autor del Post
+ *  * Opcion 1
+ *  * Opcion 2
+ *
+ * @export
+ * @interface IAuthor
+ */
 export interface IAuthor {
   name: string;
   email: string;
